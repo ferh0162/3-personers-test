1e904c8ef9e98abaf1d39225b644197a18503bca
ferh0162 <ferh0162@stud.kea.dk>
2022-03-11 14:01:03 +0100

    oprettet en metode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
 public class Main {
+    public void hey(){
+        System.out.println("Hej med dig");
+    }
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello dear");
         System.out.println("hello");
         System.out.println("Yii");
+Main obj = new Main();
+obj.hey();
     }
 }
